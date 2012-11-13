6fbbd999e7aeff93589a266efaa824eeec5a8ae4
eddiekennedy <eddiekennedy@gmail.com>
2012-11-13 00:11:18 -0500

    a rather major overhaul.

--- assets/js/nojoke.js
+++ assets/js/nojoke.js
@@ -1,31 +1,54 @@
+// * 
+// *  author: @eddiekennedy
+// *
 (function( $ ){
-  // *
-	// Define twitter feed
-    var twitterFeed =  window.location.href + 'assets/php/twitter.php';
-    // Grab tweets
-   	$.getJSON( twitterFeed, function( tweets ) {
-      // Define the list
-      var $tweetUl = $('<ul class="vertical tight tweet-list" />');
-      // Loop over tweets
-      $.each( tweets, function( i, tweet) {
-        // Define list items and append to list
-        console.log( tweet)
-        var $tweetLi = [
-          '<li>',
+  // Date formatting
+  function formatDate( date ) {
+    var newDate = new Date ( date ),
+        weekday = newDate.toString().substr(0,3),
+        day = newDate.getDate(),
+        month = parseInt( newDate.getMonth(), 0 ) + 1,
+        year = newDate.getFullYear().toString().substr(2, 2),
+        hours = newDate.getHours(),
+        minutes = newDate.getMinutes();
+    // Determine AM/PM + Adjust hours
+    amOrPm = hours < 12 ? "AM" : "PM";
+    hours = hours < 12 ? hours : hours - 12;
+    hours = hours === 0 ? 12 : hours;
+    // Tack a zero on, if needed
+    day = day < 10 ? "0" + day : day;
+    month = month < 10 ? "0" + month : month;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    // Return formatted string
+    return month + "/" + day + "/" + year + " at " + hours + ":" + minutes + amOrPm;
+  };
+	// Define twitter feed url
+  var twitterFeed =  window.location.href + 'assets/php/twitter.php';
+  // Grab tweets
+ 	$.getJSON( twitterFeed, function( tweets ) {
+    // Define the list
+    var $tweetUl = $('<ul class="vertical tight tweet-list" />');
+    // Loop over tweets
+    $.each( tweets, function( i, tweet) {
+      // Define list items and append to list
+      var createdAt = formatDate( tweet.created_at );
+      var $tweetLi = [
+        '<li>',
+          '<span class="tweet">',
             tweet.text,
-            ' <span class="date">[ ',
-              '<a href="#">',
-                tweet.created_at,
-              '</a> ]',
-            '</span>',
-          '</li>'
-        ].join('');
-        //var $tweetLi = $('<li/>').html( $liContent );
-        $tweetUl.append( $tweetLi );
-      });
-      // Remove loading message
-      $('p.loading').remove();
-      // Append list to the pge
-      $('.section.twitter').append( $tweetUl );
+          '</span>',
+          ' <span class="date">&times; ',
+            '<a href="#">',
+              createdAt,
+            '</a>',
+          '</span>',
+        '</li>'
+      ].join('');
+      $tweetUl.append( $tweetLi );
     });
+    // Remove loading message
+    $('p.loading').remove();
+    // Append list to the pge
+    $('.section.twitter').append( $tweetUl );
+  });
 }( jQuery ));
--- assets/php/twitter.php
+++ assets/php/twitter.php
@@ -1,9 +1,9 @@
 <?php
 require_once('twitteroauth/twitteroauth/twitteroauth.php');
-$key = "rlZxrboPrprkomatHOnmBA";
-$key_secret = "CmKseNSFsWNnXolPTs9fxge691ek3eutQGzN4AXoE";
-$token = "126074356-JRn99bLnvOhGhOTK4LaK1oFBKJ8Z9b53YlW9V0Js";
-$token_secret = "vl1yoVK5jLezg1Znw8bVdIU1LDbKm3wqVnSIapLZAU";
+$key = "";
+$key_secret = "";
+$token = "";
+$token_secret = "";
 $connection = new TwitterOAuth($key, $key_secret, $token, $token_secret);
 $feed = $connection->get('statuses/user_timeline',
 	array(
