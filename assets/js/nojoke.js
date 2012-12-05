e9e32b2c6231cc5586a5d2d2bbc48246a6b6df0a
eddiekennedy <eddiekennedy@gmail.com>
2012-12-04 23:37:42 -0500

    Added link parsing to twitter feed

--- assets/js/nojoke.js
+++ assets/js/nojoke.js
@@ -2,6 +2,25 @@
 // *  author: @eddiekennedy
 // *
 (function( $ ){
+  // String functions via:
+  // http://www.simonwhatley.co.uk/parsing-twitter-usernames-hashtags-and-urls-with-javascript
+  String.prototype.parseLinks = function() {
+    return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&~\?\/.=]+/g, function( url ) {
+      return url.link( url );
+    });
+  };
+  String.prototype.parseUsers = function() {
+    return this.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
+      var username = u.replace("@","")
+      return u.link("http://twitter.com/"+username);
+    });
+  };
+  String.prototype.parseHash = function() {
+    return this.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
+      var tag = t.replace("#","%23")
+      return t.link("http://search.twitter.com/search?q="+tag);
+    });
+  };
   // Date formatting
   function formatDate( date ) {
     var newDate = new Date ( date ),
@@ -21,21 +40,22 @@
     minutes = minutes < 10 ? "0" + minutes : minutes;
     // Return formatted string
     return month + "/" + day + "/" + year + " at " + hours + ":" + minutes + amOrPm;
-  };
+  }
 	// Define twitter feed url
   var twitterFeed =  window.location.href + 'assets/php/twitter.php';
   // Grab tweets
- 	$.getJSON( twitterFeed, function( tweets ) {
+  $.getJSON( twitterFeed, function( tweets ) {
     // Define the list
     var $tweetUl = $('<ul class="vertical tight tweet-list" />');
     // Loop over tweets
     $.each( tweets, function( i, tweet) {
       // Define list items and append to list
+      var theTweet = tweet.text.parseLinks().parseUsers().parseHash();
       var createdAt = formatDate( tweet.created_at );
       var $tweetLi = [
         '<li>',
           '<span class="tweet">',
-            tweet.text,
+            theTweet,
           '</span>',
           ' <span class="date">&times; ',
             '<a href="#">',
