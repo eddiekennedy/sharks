96402b5a4e679381c8099e2bcff3662a19c29256
Eddie <eddiekennedy@gmail.com>
2017-10-09 17:30:51 -0600

    removing sprite stuff

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var inlinesource = require('gulp-inline-source');
 var server = require('gulp-server-livereload');
-var sprite = require('gulp-sprite-generator');
 
 // Sass
 gulp.task('sass', function () {
@@ -14,21 +13,6 @@ gulp.task('sass', function () {
     	//outputStyle: 'compressed'
     }).on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'));
-});
-
-// Sprite
-gulp.task('sprites', function() {
-    var spriteOutput;
- 
-    spriteOutput = gulp.src("./src/css/*.css")
-      .pipe(sprite({
-        baseUrl: './src/sprite',
-        spriteSheetName: 'sharks.png',
-        spriteSheetPath: '/dist/images'
-      }));
- 
-    spriteOutput.css.pipe(gulp.dest('./dist/css'));
-    spriteOutput.img.pipe(gulp.dest('./dist/images'));
 });
 
 // Inline Source
@@ -59,6 +43,3 @@ gulp.task('watch', function () {
 
 // Default Task
 gulp.task('default', ['server', 'watch']);
-
-// Sprites Task
-gulp.task('sprite', ['sprites']);
